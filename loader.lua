31b945479272199da6ea5d413821a3c5b148a7fd
marcelulatowski239-png <marcelulatowski239@gmail.com>
2026-03-16 19:17:33 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,19 +1,22 @@
 local type = ... -- Przekazujemy "bot" lub "player"
 local key = _G.Key or ""
 
+-- TWOJE LINKI Z GITHUBA
+local bot_script = "https://raw.githubusercontent.com/marcelulatowski239-png/DataHub-V2/refs/heads/main/bot.lua"
+local gui_script = "https://raw.githubusercontent.com/marcelulatowski239-png/DataHub-V2/refs/heads/main/gui.lua"
+
 if type == "player" then
-    -- Sprawdzanie klucza tylko dla Ciebie
+    -- Sprawdzanie klucza przez Twój serwer Node.js
     local res = game:HttpGet("http://localhost:3000/check-key?key="..key)
     if not res:find("true") then 
-        return print("BŁĄD: Brak licencji! Wygeneruj klucz w KeyAuth.") 
+        return warn("Błąd licencji! Sprawdź klucz w KeyAuth.") 
     end
     
-    -- Ładowanie Twojego GUI
-    loadstring(game:HttpGet("LINK_RAW_DO_TWOJEGO_GUI_LUA"))()
-    print("✅ GUI Gracza załadowane pomyślnie!")
+    loadstring(game:HttpGet(gui_script))()
+    print("✅ GUI Gracza załadowane!")
 
 elseif type == "bot" then
-    -- Boty wchodzą bez sprawdzania klucza
-    loadstring(game:HttpGet("LINK_RAW_DO_TWOJEGO_BOTA_LUA"))()
-    print("🤖 Bot uruchomiony w trybie skanowania.")
+    -- Boty wchodzą bez klucza
+    loadstring(game:HttpGet(bot_script))()
+    print("🤖 Bot skaner uruchomiony!")
 end
